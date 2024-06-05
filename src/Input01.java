7ad5f1eeefbd728f2bd033b79f1abd80e7e4d71c
Aluno <Aluno@10.85.198.36>
2024-06-05 17:01:13 -0300

    int

---
+++ src/Input01.java
@@ -0,0 +1,19 @@
+public class Input01 {
+    public static void main(String[] args) {
+        //Create a JOptionPane.
+        //Store the input as a String and print it.
+        
+      joptionpane.showInputdialog("coloque seu nome "); 
+        //Parse the input as an int.
+        //Print its value +1
+        
+class joptionpane {
+
+    static void showInputdialog(String coloque_seu_nome_) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    
+    }
+}
+    }
+}
+
